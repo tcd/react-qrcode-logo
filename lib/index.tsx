788cc2cc117d94a9431ed825e46977a5351582aa
Clay Dunston <claydunston@gmail.com>
2022-09-10 19:48:58 -0500

    Add comments for intellisense.

--- lib/index.tsx
+++ lib/index.tsx
@@ -16,22 +16,86 @@ type InnerOuterRadii = {
 }
 
 export interface IProps {
+    /**
+     * The value encoded in the QR Code.
+     * 
+     * When the QR Code is decoded, this value will be returned.
+     */
     value?: string;
+    /**
+     * The error correction level of the QR Code.
+     */
     ecLevel?: 'L' | 'M' | 'Q' | 'H';
+    /**
+     * Enable crossorigin attribute.
+     */
     enableCORS?: boolean;
+    /**
+     * The size of the QR Code.
+     */
     size?: number;
+    /**
+     * The size of the quiet zone around the QR Code. 
+     * 
+     * This will have the same color as QR Code bgColor.
+     */
     quietZone?: number;
+    /**
+     * Background color.
+     */
     bgColor?: string;
+    /**
+     * Foreground color.
+     */
     fgColor?: string;
+    /**
+     * The logo image. 
+     * 
+     * It can be a url/path or a base64 value.
+     */
     logoImage?: string;
+    /**
+     * Logo image width.
+     */
     logoWidth?: number;
+    /**
+     * Logo image height.
+     */
     logoHeight?: number;
+    /**
+     * Logo opacity. 
+     * 
+     * This allows you to modify the transparency of your logo, so that it won't compromise the readability of the QR Code.
+     */
     logoOpacity?: number;
+    /**
+     * Removes points behind the logo and adds a border with no points around the logo.
+     */
     removeQrCodeBehindLogo?: boolean;
+    /**
+     * The corner radius for the positional patterns (the three "eyes" around the QR code). 
+     * 
+     * [See more details here](https://github.com/gcoro/react-qrcode-logo/blob/master/res/eyeRadius_doc.md).
+     */
     eyeRadius?: CornerRadii | [CornerRadii, CornerRadii, CornerRadii];
+    /**
+     * The color for the positional patterns (the three "eyes" around the QR code). 
+     * [See more details here](https://github.com/gcoro/react-qrcode-logo/blob/master/res/eyeColor_doc.md).
+     */
     eyeColor?: EyeColor | [EyeColor, EyeColor, EyeColor];
+    /**
+     * Style of the QR Code modules.
+     */
     qrStyle?: 'squares' | 'dots';
+    /**
+     * FIXME: [unused?](https://github.com/gcoro/react-qrcode-logo/blob/master/lib/index.tsx#L365)
+     */
     style?: object;
+    /**
+     * Optional custom id for the QRCode canvas. 
+     * 
+     * You can use this prop if you have multiple QRCodes and need to differentiate them.
+     */
     id?: string;
 }
 
